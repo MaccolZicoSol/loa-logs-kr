f25beffc3faeb19098b58952c24ae76c92208963
MaccolZicoSol <182187533+MaccolZicoSol@users.noreply.github.com>
2024-07-24 21:11:47 +0900

    korean update

--- src/lib/components/shared/MissingInfo.svelte
+++ src/lib/components/shared/MissingInfo.svelte
@@ -9,8 +9,8 @@
         class="text-left font-normal text-red-400 whitespace-nowrap"
         use:tooltip={{
             content:
-                "MISSING INFO FOR LOCAL PLAYER, DATA WILL BE INCORRECT. Please go to CHARACTER SELECT or SWAP CHARACTERS."
+                "미터기를 늦게 켜서 로컬 플레이어 정보가 없습니다. 정보를 다시 불러올려면 다른 캐릭터로 접속 또는 캐선창에서 다시 접속하십시오."
         }}>
-        WARNING: INVALID DATA
+        경고: 정보 없음
     </div>
 {/if}
